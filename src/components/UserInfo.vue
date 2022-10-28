58c2d93684f97e429b0ba9162f57f0c53bc0befe
arojas32 <61888719+arojas32@users.noreply.github.com>
2022-10-28 18:58:51 -0500

    change styling header to its own component

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,12 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
@@ -15,14 +17,6 @@ export default {
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
 section div {
   display: flex;
   justify-content: space-between;
